71d748f6c3fa33cea557c499f0b319f3cee73e3b
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-03-30 12:26:10 -0600

    Create MainActivity.java

---
+++ app/src/main/java/joseluis/ch/alfaware_schoolcommunicationapp/MainActivity.java
@@ -0,0 +1,16 @@
+package joseluis.ch.alfaware_schoolcommunicationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+    }
+}
