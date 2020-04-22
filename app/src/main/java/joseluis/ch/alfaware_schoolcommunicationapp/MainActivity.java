6f84888504c92fd22507d504c6ca063bc68fde0c
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-04-22 13:09:43 -0500

    Update MainActivity.java

--- app/src/main/java/joseluis/ch/alfaware_schoolcommunicationapp/MainActivity.java
+++ app/src/main/java/joseluis/ch/alfaware_schoolcommunicationapp/MainActivity.java
@@ -2,15 +2,31 @@ package joseluis.ch.alfaware_schoolcommunicationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        btnLogin = (Button)findViewById(R.id.btnIniciar);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ConteinerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
