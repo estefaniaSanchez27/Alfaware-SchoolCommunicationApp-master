dc692a7df1722bcdb8357d2c917e49b6a23c8e94
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-04-22 13:09:37 -0500

    Create MessagesFragment.java

---
+++ app/src/main/java/joseluis/ch/alfaware_schoolcommunicationapp/Fragments/MessagesFragment.java
@@ -0,0 +1,32 @@
+package joseluis.ch.alfaware_schoolcommunicationapp.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import joseluis.ch.alfaware_schoolcommunicationapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MessagesFragment extends Fragment {
+
+
+    public MessagesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_messages, container, false);
+    }
+
+}
